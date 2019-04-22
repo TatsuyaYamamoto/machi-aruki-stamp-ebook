dba962c254f1ebe70b014b799ead46bfac9fafde
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-04-22 19:32:48 +0900

    style to full height map in window

--- app/components/pages/MapPage.tsx
+++ app/components/pages/MapPage.tsx
@@ -5,6 +5,11 @@ import styled from "styled-components";
 import GoogleMap from "../organisms/GoogleMap";
 import BottomNavigation from "../organisms/BottomNavigation";
 
+const MapContainer = styled.div`
+  // TODO load '56px' of bottom nav height from material-ui theme
+  height: calc(100% - 56px);
+`;
+
 const StyledBottomNav = styled(BottomNavigation)`
   position: fixed;
   bottom: 0;
@@ -14,12 +19,14 @@ const StyledBottomNav = styled(BottomNavigation)`
 const MapPage = () => {
   return (
     <>
-      <GoogleMap
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+      <MapContainer>
+        <GoogleMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </MapContainer>
       <StyledBottomNav />
     </>
   );
