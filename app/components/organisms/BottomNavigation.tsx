a0678758846bd10d8fe3b3e0ea5613a5e162cd3e
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-04-22 19:49:33 +0900

    handle path with bottom nav

--- app/components/organisms/BottomNavigation.tsx
+++ app/components/organisms/BottomNavigation.tsx
@@ -1,38 +1,68 @@
 import * as React from "react";
+const { useState } = React;
+import { withRouter, RouteComponentProps } from "react-router-dom";
 
-import BottomNavigation from "@material-ui/core/BottomNavigation";
+import MuiBottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
+import MapIcon from "@material-ui/icons/Map";
+import DoneIcon from "@material-ui/icons/Done";
 import RestoreIcon from "@material-ui/icons/Restore";
+import * as H from "history";
 
-class SimpleBottomNavigation extends React.Component {
-  public state = {
-    value: 0
-  };
-
-  public handleChange = (event: React.ChangeEvent<{}>, value: any) => {
-    this.setState({ value });
-  };
-
-  public render() {
-    const { ...others } = this.props;
-    const { value } = this.state;
-
-    return (
-      <BottomNavigation
-        value={value}
-        onChange={this.handleChange}
-        showLabels={true}
-        {...others}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    );
+const toPath = (index: number): string => {
+  if (index === 0) {
+    return "/map";
   }
-}
+  if (index === 1) {
+    return "/stamp";
+  }
+  if (index === 2) {
+    return "/todo"; // TODO
+  }
+};
 
-export default SimpleBottomNavigation;
+const toIndex = (path: string): number => {
+  if (path.startsWith("/map")) {
+    return 0;
+  }
+  if (path.startsWith("/stamp")) {
+    return 1;
+  }
+  if (path.startsWith("/todo" /*TODO*/)) {
+    return 2;
+  }
+
+  throw new Error(`unexpected path is handled. path: ${path}`);
+};
+
+const BottomNavigation: React.FC<RouteComponentProps> = props => {
+  const { history, location, match, staticContext, ...others } = props;
+  const currentPath = location.pathname;
+
+  const [navIndex, setNavIndex] = useState(toIndex(currentPath));
+
+  const onNavChanged = (event: React.ChangeEvent<{}>, newNavIndex: number) => {
+    if (newNavIndex !== navIndex) {
+      const path = toPath(newNavIndex);
+      history.push(path);
+    }
+
+    setNavIndex(newNavIndex);
+  };
+
+  return (
+    <MuiBottomNavigation
+      value={navIndex}
+      onChange={onNavChanged}
+      showLabels={true}
+      {...others}
+    >
+      <BottomNavigationAction label="Map" icon={<MapIcon />} />
+      <BottomNavigationAction label="Stamp" icon={<DoneIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<RestoreIcon />} />
+    </MuiBottomNavigation>
+  );
+};
+
+export default withRouter(BottomNavigation);
--- app/components/pages/StampPage.tsx
+++ app/components/pages/StampPage.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
 
+import styled from "styled-components";
+
 import BottomNavigation from "../organisms/BottomNavigation";
+
+const StyledBottomNav = styled(BottomNavigation)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
 
 const StampPage = () => {
   return (
     <>
       Stamp!!
-      <BottomNavigation />
+      <StyledBottomNav />
     </>
   );
 };
